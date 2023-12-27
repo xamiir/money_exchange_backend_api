3f50066c9d96ac286ca92d9d44d44a17eff64fe2
Eng abdalla <cllaahicabdi04@gmail.com>
2023-12-27 08:44:09 +0300

    update

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,18 +1,33 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-// const URI = "mongodb://127.0.0.1:27017/App_DB";
+// // const URI = "mongodb://127.0.0.1:27017/App_DB";
 
-const URI =
-  "mongodb+srv://xamiir:xamiir_10@cluster0.ifakhb3.mongodb.net/App_DB";
+// const URI =
+//   "mongodb+srv://xamiir:xamiir_10@cluster0.ifakhb3.mongodb.net/App_DB";
   
 
 
-exports.connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(URI);
-    console.log("MongoDb Connected", conn.connection.host);
-  } catch (error) {
-    console.log("Connection failed", error);
-    process.exit(1);
+// exports.connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(URI);
+//     console.log("MongoDb Connected", conn.connection.host);
+//   } catch (error) {
+//     console.log("Connection failed", error);
+//     process.exit(1);
+//   }
+// };
+
+const mongoose = require("mongoose");
+
+const URL = "mongodb+srv://xamiir:xamiir_10@cluster0.ifakhb3.mongodb.net/App_DB";
+
+exports.connectDB = async()=>{
+  try{
+    const conn = await mongoose.connect(URL);
+    console.log("MongoDB is connect ", conn.connection.host)
+
+  }catch(err){
+    console.log("mongoDB error" ,err)
+    process.exit(1)
   }
-};
+}
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 // const model = require("./model");
-const { connectDB } = require("./config/dbconnection");
+// const { connectDB } = require("./config/dbconnection");
+const {connectDB}= require("./config/dbconnection")
+
+
 const UserRoutes = require("./routes/user");
 const currenciesRouter = require('./routes/currency');
 const transactionsRouter = require('./routes/transactions');
+
 
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
-connectDB();
+// connectDB();
+connectDB()
 
 const server = express();
 
